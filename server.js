e3424299c0402acf2c31044bb96de63eb8584984
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-09-28 23:59:27 +0000

    updated reflection

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
 //Home route/path
 app.get("/", (req, res) => {
     //Monday and Tuesday Reflection
@@ -24,6 +26,8 @@ app.get("/", (req, res) => {
     output = output + "<li>Template Literals Aka Money Curly `${}`: allow for cleaner string interpolation without concatenation. </li>";
     output = output + `<li>${notation1} provides an alternative to ${notation2} for accessing object properties.</li>`;
     output = output + `<li>${operator} offer a compact alternative to if/else statements. For React, traditional if/else statements aren't allowed.</li>`;
+    output = output + ` <img src="Node_Express_Demo.png alt="code" class="img-fluid rounded-5 border border-3 border-dark">`;
+    output = output + ` <img src="Node_Express_Demo_Bootstrap.png alt="bootstrap" class="img-fluid rounded-5 border border-3 border-dark">`;
     //Wednesday
     output = output + `<h2 class="text-center">Wednesday: Arrays and Loops (Code.org)</h2>`;
     output = output + `<p>Arrays are used to store multiple values in a single variable. They can hold different data types including numbers, strings, and objects. Arrays have an index, meaning the first element is at index 0 (or the first position).</p>`;
@@ -32,6 +36,17 @@ app.get("/", (req, res) => {
     output = output + `<li>"-" : puts dashes between two items in a list</li>`;
     output = output + `<li>"join()" : this gives a list with a comma in our console </li>`;
     output = output + `<li>"length" : this gives the number of items in an array. Adding "list.length-1" keeps us from going to far left or right when are clicking or navigating our lists. </li></ul>`;
+    output = output + ` <img src="Playlist_builder.png alt="Playlist App" class="img-fluid rounded-5 border border-3 border-dark">`;
+    output= output + ` <img src="Reminder_App.png alt="Reminder App" class="img-fluid rounded-5 border border-3 border-dark">`;
+    //Thursday and Friday
+    output = output + `<h2 class="text-center">Thursday and Friday: Madlibs Project</h2>`;
+    output = output + `<p> We recreated our Madlibs project from Level 2 using the new JavaScript syntax we learned this week.</p>`;
+    output = output + `<p>We set up our repositories with many routes/paths for the different challenge levels.</p>`;
+    output = output + `<p> We also added Bootstrap to style our pages. </p>`;
+    output = output + `<p>Adding Bootstrap in the backend allowed us to style our page without creating multiple files.</p>`;
+    output = output + `<p> We also learned how to use the "public" folder to store our CSS and image files. Adding "app.use(express.static('public'));" helped us excute this.</p>`;
+    output = output + `<p> Finally, we learned how to use query parameters to pass information. It was much better than using getValue in code.org. </p>`;
+    output = output + ` <img src="Madlibs.png alt="Madlibs Project" class="img-fluid rounded-5 border border-3 border-dark">`;
     output = output + `</div>`;
 
     console.log(output)
@@ -47,7 +62,7 @@ myWednesdayPojo = {
 }
 
 //Event Listener
-app.listen(PORT), () => {
+app.listen(PORT, () => {
 console.log(`Server is running on port: ${PORT}`);
-};
+});
 
